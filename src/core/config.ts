46fdf111e6960549fe4a214f8c2687de6bccc3ad
Beardhack <lockard.ryan@gmail.com>
2025-08-15 07:44:38 -0400

    Create config.ts

---
+++ src/core/config.ts
@@ -0,0 +1,35 @@
+export const CONFIG = {
+  VERSION: "m0b",
+  SAVE_KEY: "mp_shipwrecked_mod_v1",
+  TILE_SIZE: 20,
+  MAP_W: 64,
+  MAP_H: 64,
+  VIEW_W: 40,
+  VIEW_H: 30,
+  HEALTH_MAX: 10,
+  STAM_MAX: 100,
+  HUNGER_MAX: 100,
+  HUNGER_DRAIN_PER_TURN: 1,
+  STAM_MOVE_COST: 1,
+  STAM_GATHER_COST: 2,
+  DAY_TURNS: 120,
+  CRAB_MOVE_PERIOD: 2,
+  CRAB_DAMAGE: 1,
+  TIDE_PERIOD: 50,
+  TIDE_VARIANCE: 20,
+  RESOURCE_RESPAWN: {
+    palm: 80, banana: 70, vines: 60, rock: 90, drift: 60, shell: 70, wreck: 120
+  }
+};
+
+export const COLORS = {
+  beach: "#e8d7a5",
+  jungle: "#276f3b",
+  jungle2: "#1e5a30",
+  water: "#2b5ea7",
+  rock: "#807d7a",
+  scrap: "#7a8ea1",
+  ship: "#6f4f28",
+  shell: "#d7c7bf",
+  high: "#8c8769",
+};
